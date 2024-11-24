80df2af29b11fc395582d67de275ab6372dba7d3
JapanSG <japan.sri45326@gmail.com>
2024-11-24 19:32:50 +0700

    modified skillpoint.py - fix syntax error - remove try except statements and add it as a game mechanic

--- skillpoint.py
+++ skillpoint.py
@@ -6,7 +6,7 @@ class SkillPoint:
 
         if curr > maximum:
             raise ValueError("curr can not be more than max")
-        if curr < 0 or max < 0:
+        if curr < 0 or maximum < 0:
             raise ValueError("max and curr cannot be less than 0")
 
         self.max = maximum
@@ -14,22 +14,18 @@ class SkillPoint:
     
     def __str__(self) -> str:
         '''str'''
-        return f"{self.max}"
+        return f"{self.curr}"
 
     def __repr__(self) -> int:
         '''repr'''
-        return self.max
+        return self.curr
 
     def use(self, num : int) -> None:
         '''manager skill point recovery and usage'''
         updated = self.curr + num
-        try :
-            if updated < 0:
-                self.curr = 0
-                raise ValueError
-            elif updated > self.max:
-                self.curr = self.max
-            else:
-                self.curr = updated
-        except ValueError:
-            print("Skill point can not be lower than 0")
+        if updated < 0:
+            self.curr = 0
+        elif updated > self.max:
+            self.curr = self.max
+        else:
+            self.curr = updated
