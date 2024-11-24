4591bbce8739b8eb13eb3872aef60b3ac345170e
JapanSG <japan.sri45326@gmail.com>
2024-11-24 16:45:33 +0700

    Modified character.py - Added minor information in docstring of Character constructor - Added level stats - Put dmg_boost and res into dictionaries for easier access and more organized

--- character.py
+++ character.py
@@ -1,9 +1,11 @@
 '''character'''
 class Character:
     '''Character class'''
-    def __init__(self, name :str, base_stats : tuple, **kwargs) -> None:
+    def __init__(self, name :str, level:int, base_stats : tuple, **kwargs) -> None:
         '''
         Constructor
+        name = character name : str
+        level = (1 to 80)
         base_stats =        (
                             hp:float,
                             atk:float,
@@ -44,12 +46,14 @@ class Character:
                             )
 
         other_stats =      (
+                            vulnerable : float,
                             curr_hp: float,
                             break_efficiency: float,
                             taunt: float
                             )
         '''
         self.name = name
+        self.level = level
         self.hp, self.atk, self.defe, self.spd = base_stats
         self.curr_hp = self.hp
 
@@ -62,21 +66,27 @@ class Character:
         self.effect_hit_rate = kwargs.get('effect_hit_rate', 0.0)
         self.effect_res = kwargs.get('effect_res', 0.0)
 
-        self.physical_dmg = kwargs.get('physical_dmg', 0.0)
-        self.fire_dmg = kwargs.get('fire_dmg', 0.0)
-        self.ice_dmg = kwargs.get('ice_dmg', 0.0)
-        self.lightning_dmg = kwargs.get('lightning_dmg', 0.0)
-        self.wind_dmg = kwargs.get('wind_dmg', 0.0)
-        self.quantum_dmg = kwargs.get('quantum_dmg', 0.0)
-        self.imaginary_dmg = kwargs.get('imaginary_dmg', 0.0)
+        self.dmg_boost = {
+            'physical' : kwargs.get('physical_dmg', 0.0),
+            'fire' : kwargs.get('fire_dmg', 0.0),
+            'ice' : kwargs.get('ice_dmg', 0.0),
+            'lightning' : kwargs.get('lightning_dmg', 0.0),
+            'wind' : kwargs.get('wind_dmg', 0.0),
+            'quantum' : kwargs.get('quantum_dmg', 0.0),
+            'imaginary' : kwargs.get('imaginary_dmg', 0.0)
+        }
 
-        self.physical_res = kwargs.get('physical_res', 0.0)
-        self.fire_res = kwargs.get('fire_res', 0.0)
-        self.ice_res = kwargs.get('ice_res', 0.0)
-        self.lightning_res = kwargs.get('lightning_res', 0.0)
-        self.wind_res = kwargs.get('wind_res', 0.0)
-        self.quantum_res = kwargs.get('quantum_res', 0.0)
-        self.imaginary_res = kwargs.get('imaginary_res', 0.0)
+        self.res = {
+            'physical' : kwargs.get('physical_res', 0.0),
+            'fire' : kwargs.get('fire_res', 0.0),
+            'ice' : kwargs.get('ice_res', 0.0),
+            'lightning' : kwargs.get('lightning_res', 0.0),
+            'wind' : kwargs.get('wind_res', 0.0),
+            'quantum' : kwargs.get('quantum_res', 0.0),
+            'imaginary' : kwargs.get('imaginary_res', 0.0)
+        }
+
+        self.vulnerable = kwargs.get('vulnerable', 0.0)
 
     def __str__(self) -> str:
         '''String'''
