71eb77f3897d592585e016b70d11093ad425fb7b
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-12-21 21:17:18 +0700

    TODO: Add help menu in `turn_order_manager`

--- character.py
+++ character.py
@@ -1,16 +1,27 @@
-'''character_base_class'''
+"""character_base_class"""
+
 import event
+
+
 class Character:
-    '''Character class'''
-    def __init__(self, name :str, level:int, base_stats : tuple, observer : event.EventManager, **kwargs) -> None:
-        '''
+    """Character class"""
+
+    def __init__(
+        self,
+        name: str,
+        level: int,
+        base_stats: tuple,
+        observer: event.EventManager,
+        **kwargs,
+    ) -> None:
+        """
         Constructor
         name = character name : str
         level = (1 to 80)
         base_stats =        (
                             hp:float,
                             atk:float,
-                            defe:float,
+                            def:float,
                             spd:float
                             )
 
@@ -52,81 +63,85 @@ class Character:
                             break_efficiency: float,
                             taunt: float
                             )
-        '''
+        """
 
         self.stats = {
-            "hp"                : base_stats[0],
-            "hp_per"            : kwargs.get('hp_per', 0.0),
-            "hp_flat"           : kwargs.get('hp_flat', 0.0),
-
-            "atk"               : base_stats[1],
-            "atk_per"           : kwargs.get("atk_per", 0.0),
-            "atk_flat"          : kwargs.get("atk_flat", 0.0),
-
-            "defe"              : base_stats[2],
-            "def_per"           : kwargs.get("def_per", 0.0),
-            "def_flat"          : kwargs.get("def_flat", 0.0),
-
-            "spd"               : base_stats[3],
-            "spd_per"           : kwargs.get("spd_per", 0.0),
-            "spd_flat"          : kwargs.get("spd_flat", 0.0),
-
-            "crit_rate"         : kwargs.get('crit_rate', 0.05),
-            "crit_dmg"          : kwargs.get('crit_dmg', 0.50),
-            "break_effect"      : kwargs.get('break_effect', 0.0),
-            "outgoing_healing"  : kwargs.get('outgoing_healing', 0.0),
-            "max_energy"        : kwargs.get('max_energy', 0.0),
-            "energy_regen_rate" : kwargs.get('energy_regen_rate', 0.0),
-            "effect_hit_rate"   : kwargs.get('effect_hit_rate', 0.0),
-            "effect_res"        : kwargs.get('effect_res', 0.0),
-
-            "def_reduction"     : kwargs.get('def_reduction', 0.0),
-            "def_ignore"        : kwargs.get('def_ignore', 0.0)
+            "hp": base_stats[0],
+            "hp_per": kwargs.get("hp_per", 0.0),
+            "hp_flat": kwargs.get("hp_flat", 0.0),
+            "atk": base_stats[1],
+            "atk_per": kwargs.get("atk_per", 0.0),
+            "atk_flat": kwargs.get("atk_flat", 0.0),
+            "def": base_stats[2],
+            "def_per": kwargs.get("def_per", 0.0),
+            "def_flat": kwargs.get("def_flat", 0.0),
+            "spd": base_stats[3],
+            "spd_per": kwargs.get("spd_per", 0.0),
+            "spd_flat": kwargs.get("spd_flat", 0.0),
+            "crit_rate": kwargs.get("crit_rate", 0.05),
+            "crit_dmg": kwargs.get("crit_dmg", 0.50),
+            "break_effect": kwargs.get("break_effect", 0.0),
+            "outgoing_healing": kwargs.get("outgoing_healing", 0.0),
+            "max_energy": kwargs.get("max_energy", 0.0),
+            "energy_regen_rate": kwargs.get("energy_regen_rate", 0.0),
+            "effect_hit_rate": kwargs.get("effect_hit_rate", 0.0),
+            "effect_res": kwargs.get("effect_res", 0.0),
+            "def_reduction": kwargs.get("def_reduction", 0.0),
+            "def_ignore": kwargs.get("def_ignore", 0.0),
         }
 
         self.name = name
         self.level = level
 
-        self.total_hp = self.stats["hp"] * (1 + self.stats["hp_per"]) + self.stats["hp_flat"]
+        self.total_hp = (
+            self.stats["hp"] * (1 + self.stats["hp_per"]) + self.stats["hp_flat"]
+        )
         self.curr_hp = self.total_hp
 
-        self.total_atk = self.stats["atk"] * (1 + self.stats["atk_per"]) + self.stats["atk_flat"]
+        self.total_atk = (
+            self.stats["atk"] * (1 + self.stats["atk_per"]) + self.stats["atk_flat"]
+        )
 
-        self.total_def = self.stats["defe"] * (1 + self.stats["def_per"] - self.stats["def_ignore"]) + self.stats["def_flat"]
+        self.total_def = (
+            self.stats["def"] * (1 + self.stats["def_per"] - self.stats["def_ignore"])
+            + self.stats["def_flat"]
+        )
 
-        self.total_spd = self.stats["spd"] * (1 + self.stats["spd_per"]) + self.stats["spd_flat"]
+        self.total_spd = (
+            self.stats["spd"] * (1 + self.stats["spd_per"]) + self.stats["spd_flat"]
+        )
 
-        self.dot_dmg_boost = kwargs.get('dot_dmg_boost', 0.0)
-        self.dot_dmg_taken = kwargs.get('dot_dmg_taken', 0.0)
+        self.dot_dmg_boost = kwargs.get("dot_dmg_boost", 0.0)
+        self.dot_dmg_taken = kwargs.get("dot_dmg_taken", 0.0)
 
         self.dmg_boost = {
-            'physical' : kwargs.get('physical_dmg', 0.0),
-            'fire' : kwargs.get('fire_dmg', 0.0),
-            'ice' : kwargs.get('ice_dmg', 0.0),
-            'lightning' : kwargs.get('lightning_dmg', 0.0),
-            'wind' : kwargs.get('wind_dmg', 0.0),
-            'quantum' : kwargs.get('quantum_dmg', 0.0),
-            'imaginary' : kwargs.get('imaginary_dmg', 0.0)
+            "physical": kwargs.get("physical_dmg", 0.0),
+            "fire": kwargs.get("fire_dmg", 0.0),
+            "ice": kwargs.get("ice_dmg", 0.0),
+            "lightning": kwargs.get("lightning_dmg", 0.0),
+            "wind": kwargs.get("wind_dmg", 0.0),
+            "quantum": kwargs.get("quantum_dmg", 0.0),
+            "imaginary": kwargs.get("imaginary_dmg", 0.0),
         }
 
         self.res = {
-            'physical' : kwargs.get('physical_res', 0.0),
-            'fire' : kwargs.get('fire_res', 0.0),
-            'ice' : kwargs.get('ice_res', 0.0),
-            'lightning' : kwargs.get('lightning_res', 0.0),
-            'wind' : kwargs.get('wind_res', 0.0),
-            'quantum' : kwargs.get('quantum_res', 0.0),
-            'imaginary' : kwargs.get('imaginary_res', 0.0)
+            "physical": kwargs.get("physical_res", 0.0),
+            "fire": kwargs.get("fire_res", 0.0),
+            "ice": kwargs.get("ice_res", 0.0),
+            "lightning": kwargs.get("lightning_res", 0.0),
+            "wind": kwargs.get("wind_res", 0.0),
+            "quantum": kwargs.get("quantum_res", 0.0),
+            "imaginary": kwargs.get("imaginary_res", 0.0),
         }
 
-        self.vulnerable = kwargs.get('vulnerable', 0.0)
+        self.vulnerable = kwargs.get("vulnerable", 0.0)
 
         self.observer = observer
 
     def __str__(self) -> str:
-        '''String'''
+        """String"""
         return f"{self.name}"
 
     def __repr__(self) -> str:
-        '''repr'''
+        """repr"""
         return self.__str__()
--- turn_order_manager.py
+++ turn_order_manager.py
@@ -1,95 +1,104 @@
-'''turn_order_manager'''
+"""turn_order_manager"""
+
 import skillpoint as sk
 import ally_enemy_base
 import ally as a
 import enemy as e
 import math as m
+
+
 # TODO
 # list of things to fix
 # everyting is broken since structuring inheritance in the Character class and it's subclasses
 # - do_action function
 # - remove_dead function
 class TurnManager:
-    '''
+    """
     TurnManager class
     Manages characters turn order
     and action values
-    '''
-    def __init__(self, sp : sk.SkillPoint, *characters : ally_enemy_base.Character) -> None:
-        '''
+    """
+
+    def __init__(
+        self, sp: sk.SkillPoint, *characters: ally_enemy_base.Character
+    ) -> None:
+        """
         Constructor
         self.order values = [character.Character, AG left, Current AV]
-        '''
+        """
         self.sp = sp
         self.running = True
         self.allies = [char for char in characters if isinstance(char, a.Ally)]
         self.enemies = [char for char in characters if isinstance(char, e.Enemy)]
-        self.order = [[char, 10000, m.ceil(10000/char.total_spd)] for char in characters] # item = [Character, AG left, current AV]
-        self.order.sort(key = lambda item : item[2])
+        self.order = [
+            [char, 10000, m.ceil(10000 / char.total_spd)] for char in characters
+        ]  # item = [Character, AG left, current AV]
+        self.order.sort(key=lambda item: item[2])
 
     def update(self) -> None:
-        '''Update turn order'''
+        """Update turn order"""
         av_passed = self.order[0][2]
         if not av_passed:
             item = self.order.pop(0)
             self.do_action(item)
-            new = [item[0], 10000, m.ceil(10000/item[0].total_spd)]
+            new = [item[0], 10000, m.ceil(10000 / item[0].total_spd)]
             self.__insert(new)
             return
         for i in range(len(self.order)):
             item = self.order[i]
             total_spd = item[0].total_spd
-            ag = max(item[1] - total_spd*av_passed, 0)
+            ag = max(item[1] - total_spd * av_passed, 0)
             av = max(item[2] - av_passed, 0)
             self.order[i] = [item[0], ag, av]
 
-    def print_order(self, curr_ally_name) -> None: 
-        '''Print Order'''
+    def print_order(self, curr_ally_name) -> None:
+        """Print Order"""
         print(f"{curr_ally_name:<20s}| Doing action")
         for item in self.order:
             print(f"{item[0].name:<20s}| {item[2]} AV")
 
     def __str__(self) -> str:
-        '''String'''
+        """String"""
         lis = [[item[0].name, item[2], item[1]] for item in self.order]
         return f"{lis}"
 
     def do_action(self, item) -> None:
-        '''Do action when av == 0 and reset av'''
-        #TODO
+        """Do action when av == 0 and reset av"""
+        # TODO
         if isinstance(item[0], ally_enemy_base.Ally):
             self.input_action(item[0])
         else:
             print("Enemy took turn")
         return print("Pass")
-    
-    def input_action(self, ally : ally_enemy_base.Ally):
+
+    def input_action(self, ally: ally_enemy_base.Ally):
         finished = False
         print(f"It's {ally.name} turn")
         while not finished:
             command = input("Please input command -> ")
-            if command == 'q':
+            if command == "q":
                 print("You have quit the program")
                 self.running = False
                 finished = True
-            elif command == 'help':
-                print("List of Commands :")
-            elif command == 'order':
+            elif command == "help":
+                # TODO Add list of commands
+                print("List of Commands:")
+            elif command == "order":
                 self.print_order(ally.name)
-            elif command == 'hp':
+            elif command == "hp":
                 for enemy in self.enemies:
                     print(enemy.curr_hp)
-            elif command == 'va':
+            elif command == "va":
                 print(*self.allies)
-            elif command == 've':
+            elif command == "ve":
                 print(*self.enemies)
-            elif command == 'sp':
+            elif command == "sp":
                 print(self.sp)
             else:
                 finished = ally.input_action(command, self.enemies, self.sp)
 
     def __insert(self, item: list) -> None:
-        '''insert item at correct av'''
+        """insert item at correct av"""
         for i in range(len(self.order)):
             if self.order[i][2] > item[2]:
                 self.order.insert(i, item)
@@ -98,7 +107,7 @@ class TurnManager:
 
     # FIXME
     def remove_dead(self) -> None:
-        '''remove character that have less than  0 hp'''
+        """remove character that have less than  0 hp"""
         dead_allies = []
         for i in range(len(self.allies)):
             if self.allies[i].curr_hp <= 0:
@@ -114,7 +123,7 @@ class TurnManager:
             self.enemies.pop(i)
 
     def loop(self) -> None:
-        '''Main loop'''
+        """Main loop"""
         while True:
             if not self.running:
                 break
@@ -128,8 +137,10 @@ class TurnManager:
             self.update()
         print("end")
 
+
 def main():
-    '''Driver Code'''
+    """Driver Code"""
+
 
 if __name__ == "__main__":
     main()
