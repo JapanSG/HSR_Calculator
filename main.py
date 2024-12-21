6022e64ecc936326723a93142d88f6108fc137f1
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-12-22 03:18:50 +0700

    Integrated `DamageListener` to `main.py`

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-'''main'''
-from ally import DestructionTrailblazer
+"""main"""
+
+from ally import DestructionTrailblazer, DamageListener
 import ally_enemy_base as ae
 from event import EventManager
 from skillpoint import SkillPoint
 from turn_order_manager import TurnManager
+
+
 def main():
-    '''Driver Code'''
+    """Driver Code"""
+    damage_listener = DamageListener()
     event_manager = EventManager()
+    event_manager.attach(damage_listener)
     sp = SkillPoint()
     mc = DestructionTrailblazer(80, event_manager)
     event_manager.attach(mc)
-    enemy1 = ae.Enemy("enemy1", 80, (5000,300,300,100), event_manager)
+    enemy1 = ae.Enemy("enemy1", 80, (5000, 300, 300, 100), event_manager)
     event_manager.attach(enemy1)
-    turn_manager = TurnManager(sp ,mc, enemy1)
+    turn_manager = TurnManager(sp, mc, enemy1)
     turn_manager.loop()
+
+
 if __name__ == "__main__":
     main()
